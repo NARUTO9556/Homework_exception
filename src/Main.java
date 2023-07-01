91cf89d60af04a2d960e057acd229eb51329be63
NARUTO9556 <gruver5263@gmail.com>
2023-07-01 22:27:53 +0800

    дз по исключениям

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkLoginAndPassword("login","password","password");
+    }
+
+    public static void checkLoginAndPassword(String login, String password, String confirmPassword) {
+        try {
+            checkLogin(login);
+            checkPassword(password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("Проверка завершена");
+        }
+    }
+    private static void checkLogin(String login) throws WrongLoginException {
+        Pattern p = Pattern.compile("^[a-z0-9_-]{1,20}$");
+        if (!p.matcher(login).matches()) {
+            throw new WrongLoginException("Логин не подходит под требования " + login);
+        }
+    }
+    private static void checkPassword(String password, String confirmPassword) throws WrongPasswordException {
+        Pattern p = Pattern.compile("^[a-z0-9_-]{1,20}$");
+        if (!p.matcher(password).matches()) {
+            throw new WrongPasswordException("Пароль не подходит под требования " + password);
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
     }
 }
